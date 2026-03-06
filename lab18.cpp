13580df6e4c796084d72a91fb851a6d104e7648d
Yang Liu <summerliu2333@gmail.com>
2026-03-05 17:55:01 -0800

    addToHead & addToTail

--- lab18.cpp
+++ lab18.cpp
@@ -9,6 +9,37 @@ struct ReviewNode {
     ReviewNode* next;
 };
 
+ReviewNode* addToHead(ReviewNode* head, double rating, const char* comment); 
+ReviewNode* addToTail(ReviewNode* head, double rating, const char* comment); 
+double printReviews(ReviewNode* head);                                       
+void freeList(ReviewNode*& head);  
+
 int main() {
     return 0;
+}
+
+ReviewNode* addToHead(ReviewNode* head, double rating, const char* comment) {
+    ReviewNode* newNode = new ReviewNode();
+    newNode->rating = rating;
+    strcpy(newNode->comment, comment);
+    newNode->next = head;
+    return newNode;
+}
+
+ReviewNode* addToTail(ReviewNode* head, double rating, const char* comment) {
+    ReviewNode* newNode = new ReviewNode();
+    newNode->rating = rating;
+    strcpy(newNode->comment, comment);
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        return newNode;
+    }
+
+    ReviewNode* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+    current->next = newNode;
+    return head;
 }
