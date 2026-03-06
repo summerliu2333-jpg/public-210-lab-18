aa83fb312066974605480ed79750e85cff684bfd
Yang Liu <summerliu2333@gmail.com>
2026-03-05 18:01:34 -0800

    Implement Loop

--- lab18.cpp
+++ lab18.cpp
@@ -15,6 +15,39 @@ double printReviews(ReviewNode* head);
 void freeList(ReviewNode*& head);  
 
 int main() {
+    cout << "Which linked list method should we use?" << endl;
+    cout << "    [1] New nodes are added at the head of the linked list" << endl;
+    cout << "    [2] New nodes are added at the tail of the linked list" << endl;
+    cout << "Choice: ";
+
+    int choice;
+    cin >> choice;
+    cin.ignore();
+
+    ReviewNode* head = nullptr;
+    char addAnother;
+
+    do {
+        double rating;
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cin.ignore();
+
+        char comment[300];
+        cout << "Enter review comments: ";
+        cin.getline(comment, sizeof(comment));
+
+        if (choice == 1) {
+            head = addToHead(head, rating, comment);
+        } else {
+            head = addToTail(head, rating, comment);
+        }
+
+        cout << "Enter another review? Y/N: ";
+        cin >> addAnother;
+        cin.ignore();
+    } while (addAnother == 'y' || addAnother == 'Y');
+
     return 0;
 }
 
