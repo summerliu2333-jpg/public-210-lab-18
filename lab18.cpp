e39ac5767805114a221eee34961a23b7c6e71a87
Yang Liu <summerliu2333@gmail.com>
2026-03-05 17:42:03 -0800

    Initial commit

---
+++ lab18.cpp
@@ -0,0 +1,14 @@
+//COMSC-210-5068, Lab 18, Yang Liu
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct ReviewNode {
+    double rating;
+    char comment[300];
+    ReviewNode* next;
+};
+
+int main() {
+    return 0;
+}
